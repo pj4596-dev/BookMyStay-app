9b56b4185297d637134d4c863d8914bbd848ee4b
Piyush Jaiswal <pj4596@srmist.edu.in>
2026-04-07 11:02:51 +0530

    Added UC7: Add-On Service Selection

---
+++ src/bookmystay/AddOnServiceManager.java
@@ -0,0 +1,35 @@
+package bookmystay;
+
+import java.util.*;
+
+public class AddOnServiceManager {
+
+    private Map<String, List<AddOnService>> servicesByReservation;
+
+    public AddOnServiceManager() {
+        servicesByReservation = new HashMap<>();
+    }
+
+    // Add service to reservation
+    public void addService(String reservationId, AddOnService service) {
+
+        servicesByReservation
+                .computeIfAbsent(reservationId, k -> new ArrayList<>())
+                .add(service);
+    }
+
+    // Calculate total cost
+    public double calculateTotalServiceCost(String reservationId) {
+
+        List<AddOnService> services =
+                servicesByReservation.getOrDefault(reservationId, new ArrayList<>());
+
+        double total = 0;
+
+        for (AddOnService service : services) {
+            total += service.getCost();
+        }
+
+        return total;
+    }
+}
