0275105f99badb554f6665c146355950742d2770
Piyush Jaiswal <pj4596@srmist.edu.in>
2026-04-07 09:42:56 +0530

    Added UC1: Application Entry

---
+++ src/bookmystay/UseCase1HotelBookingApp.java
@@ -0,0 +1,8 @@
+package bookmystay;
+
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
