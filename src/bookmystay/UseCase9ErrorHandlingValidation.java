7e0e942150561040b4cf701cb71cc51fbddf91f8
Piyush Jaiswal <pj4596@srmist.edu.in>
2026-04-07 11:09:24 +0530

    Added UC9: Validation & Error Handling

---
+++ src/bookmystay/UseCase9ErrorHandlingValidation.java
@@ -0,0 +1,43 @@
+package bookmystay;
+
+import java.util.Scanner;
+
+public class UseCase9ErrorHandlingValidation {
+
+    public static void main(String[] args) {
+
+        System.out.println("Booking Validation");
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Initialize components
+        RoomInventory inventory = new RoomInventory();
+        ReservationValidator validator = new ReservationValidator();
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+        try {
+            // Input
+            System.out.print("Enter guest name: ");
+            String guestName = scanner.nextLine();
+
+            System.out.print("Enter room type (Single/Double/Suite): ");
+            String roomType = scanner.nextLine();
+
+            // Validate
+            validator.validate(guestName, roomType, inventory);
+
+            // If valid → create booking
+            Reservation reservation = new Reservation(guestName, roomType);
+            bookingQueue.addRequest(reservation);
+
+            System.out.println("Booking request added successfully!");
+
+        } catch (InvalidBookingException e) {
+
+            System.out.println("Booking failed: " + e.getMessage());
+
+        } finally {
+            scanner.close();
+        }
+    }
+}
